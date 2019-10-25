5c134bfb38c75cea4343d752755c4a8d0c184469
Sean Batzel <romulus108@protonmail.com>
2019-10-24 20:17:07 -0400

    Fixed an unresolved reference.

--- src/main/kotlin/se575/Block.kt
+++ src/main/kotlin/se575/Block.kt
@@ -4,6 +4,6 @@ class Block(size: Int, hash: String, txCount: Int, txList: Array<Transaction>) {
   val time: Long
 
   init {
-    time = getTimeMillis()
+    time = System.currentTimeMillis())
   }
 }
