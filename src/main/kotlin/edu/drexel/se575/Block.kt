40d3143be55e38727a45b763ea366a8485a34aa1
Sean Batzel <romulus108@protonmail.com>
2019-10-31 19:49:31 -0400

    Trying to work out how properties work in this language...

--- src/main/kotlin/edu/drexel/se575/Block.kt
+++ src/main/kotlin/edu/drexel/se575/Block.kt
@@ -1,7 +1,7 @@
 package edu.drexel.se575
 
-class Block(hash: String, txCount: Int, txList: Array<Transaction>) {
-  private val time: Long =  System.currentTimeMillis()
+class Block(public var hash: String, public var txCount: Int, public var txList: Array<Transaction>) {
+  public var time: Long =  System.currentTimeMillis()
 }
 
 fun mintNewBlock(txList: Array<Transaction>): Block {
