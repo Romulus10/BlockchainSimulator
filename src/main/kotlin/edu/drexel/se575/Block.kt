fd3ffc9abff59810eaab005ac155fac46d1587f5
Sean Batzel <romulus108@protonmail.com>
2019-10-31 19:33:17 -0400

    Because of course this language wouldn't just use the JVM's arrays...

--- src/main/kotlin/edu/drexel/se575/Block.kt
+++ src/main/kotlin/edu/drexel/se575/Block.kt
@@ -7,5 +7,5 @@ class Block(hash: String, txCount: Int, txList: Array<Transaction>) {
 fun mintNewBlock(txList: Array<Transaction>): Block {
   // TODO Generate a hash of the block - should probably implement a ToString for the Transaction object.
   val hash: String = "Placeholder"
-  return Block(hash, txList.length(), txList)
+  return Block(hash, txList.size, txList)
 }
