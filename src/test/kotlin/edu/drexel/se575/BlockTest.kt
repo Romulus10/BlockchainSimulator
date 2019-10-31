a2f39cc8822ffbd9488f0f275045e6ac14fe4f94
Sean Batzel <romulus108@protonmail.com>
2019-10-31 19:40:24 -0400

    Still fixing tests.

--- src/test/kotlin/edu/drexel/se575/BlockTest.kt
+++ src/test/kotlin/edu/drexel/se575/BlockTest.kt
@@ -10,6 +10,7 @@ import org.junit.jupiter.params.provider.CsvSource
 class BlockTests {
     @Test fun `mintNewBlock produces a valid block with the correct size`() {
         val blockTxList: Array<Transaction> = arrayOf(Transaction("test","test","Text TX"))
-        assertEquals(mintNewBlock(blockTxList).txCount, 1)
+        val block: Block = mintNewBlock(blockTxList)
+        assertEquals(block.txCount, 1)
     }
 }
