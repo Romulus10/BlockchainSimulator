cee0539fc759fc23dd7a1d7d2612299e6cac2d3a
Sean Batzel <romulus108@protonmail.com>
2019-10-31 19:24:15 -0400

    Apparently I don't understand Kotlin.

--- src/main/kotlin/edu/drexel/se575/Block.kt
+++ src/main/kotlin/edu/drexel/se575/Block.kt
@@ -2,12 +2,10 @@ package edu.drexel.se575
 
 class Block(hash: String, txCount: Int, txList: Array<Transaction>) {
   private val time: Long =  System.currentTimeMillis()
-}
 
-/**
- */
 fun mintNewBlock(txList: Array<Transaction>) -> Block {
   // TODO Generate a hash of the block - should probably implement a ToString for the Transaction object.
   val hash: String = "Placeholder"
   return Block(hash, txList.length(), txList)
+}
 }
--- src/main/kotlin/edu/drexel/se575/ProveByStake.kt
+++ src/main/kotlin/edu/drexel/se575/ProveByStake.kt
@@ -1,10 +1,2 @@
 package edu.drexel.se575
 
-/**
- * Used to determine the probability that an account will be chosen to mint a new block.
- * @param acct The address whose network staking weight to query.
- * @return The percent of network weight staked by this address.
- */
-fun getStakeWeightByAccount(acct: Account) -> Double {
-
-}
