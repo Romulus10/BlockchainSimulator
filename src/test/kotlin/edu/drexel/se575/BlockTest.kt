e02f477bf2920023757b965e7bff7a8e03cfc566
Sean Batzel <romulus108@protonmail.com>
2019-10-31 19:36:50 -0400

    Fix test errors.

--- src/test/kotlin/edu/drexel/se575/BlockTest.kt
+++ src/test/kotlin/edu/drexel/se575/BlockTest.kt
@@ -7,9 +7,9 @@ import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
-class BlockchainSimulatorTests {
+class BlockTests {
     @Test fun `mintNewBlock produces a valid block with the correct size`() {
         val blockTxList: Array<Transaction> = arrayOf(Transaction("test","test","Text TX"))
-        assertEquals(mintNewBlock(blockTxList).size, 1)
+        assertEquals(mintNewBlock(blockTxList).txCount, 1)
     }
 }
