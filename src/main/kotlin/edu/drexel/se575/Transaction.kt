127b68c82042c13526b98a53e4a3187b3e874d14
Sean Batzel <romulus108@protonmail.com>
2019-11-04 19:31:32 -0500

    Added a pair of hashing functions and some extensions to the existing classes.

--- src/main/kotlin/edu/drexel/se575/Transaction.kt
+++ src/main/kotlin/edu/drexel/se575/Transaction.kt
@@ -1,6 +1,10 @@
 package edu.drexel.se575
 
-class Transaction(public var to: String, public var fr: String, public var data: String) {
-public var block_height: Int? = null
-public var block_number: Int? = null
+class Transaction(var to: String, var fr: String, var data: String) {
+    var block_height: Int? = null
+    var block_number: Int? = null
+
+    override fun toString(): String {
+        return "To: %s, From: %s, Data: %s, Block Height: %s, Block Number: %s".format(to, fr, data, block_height, block_number)
+    }
 }
