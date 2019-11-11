423f8f6d4cf642caf86fc2fe60857352b43d8634
Sean Batzel <romulus108@protonmail.com>
2019-11-10 20:36:33 -0500

    Proof of stake (#13)
    * Started fleshing out the basics of our block creation process, and wrote some preliminary documentation.
    
    * Code formatting, and fixing some real stupid mistakes.
    
    * That was still broken quite badly...

--- src/main/kotlin/edu/drexel/se575/Account.kt
+++ src/main/kotlin/edu/drexel/se575/Account.kt
@@ -1,7 +1,13 @@
 package edu.drexel.se575
 
+/**
+ * A nice, warm place to keep your stuff and run transactions from.
+ * @property address The unique address of the account.
+ * @property weight The amount of coin held by this account.
+ */
 class Account {
-    private var address: String
+    var address: String
+    var weight: Int = 0
 
     init {
         val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
@@ -10,4 +16,19 @@ class Account {
                 .map(charPool::get)
                 .joinToString(""))
     }
+}
+
+/**
+ * Now in O(n) complexity!
+ * @return The account in accountList with the highest staking weight.
+ */
+fun maxAccountWeight(accountList: Array<Account>, voteValues: Array<Double?>): Account? {
+    var max: Account? = null
+    val maxVote = 0.0
+    for (x in accountList.indices) {
+        if (voteValues[x]!! > maxVote) {
+            max = accountList[x]
+        }
+    }
+    return max
 }
--- src/main/kotlin/edu/drexel/se575/Util.kt
+++ src/main/kotlin/edu/drexel/se575/Util.kt
@@ -5,9 +5,20 @@ import javax.xml.bind.DatatypeConverter
 
 object Util {
 
+    /**
+     * @returns The SHA-1 hash of [input].
+     */
     fun sha1(input: String) = hashString("SHA-1", input)
+
+    /**
+     * @returns The MD5 hash of [input].
+     */
     fun md5(input: String) = hashString("MD5", input)
 
+    /**
+     * Mangles the string beyond repair, but we do get a unique identifier out of it.
+     * @return The hash of [input] specified by [type].
+     */
     private fun hashString(type: String, input: String): String {
         val bytes = MessageDigest
                 .getInstance(type)
