fef12d0a8d907be36d2dd4b29b58f236a9916ae2
Sean Batzel <romulus108@protonmail.com>
2019-11-20 21:57:24 -0500

    Working on a very simple compiler for transactions, and an interpreter for reading contracts built into transactions.

--- Simulator/src/main/kotlin/edu/drexel/se575/contract/Compiler.kt
+++ Simulator/src/main/kotlin/edu/drexel/se575/contract/Compiler.kt
@@ -14,9 +14,36 @@ class Compiler {
         var retval = ""
         val intermediate = line.split(' ')
         when (intermediate[0]) {
-            "move" -> retval + "0"
+            "nop" -> {
+                retval += "0-"
+            }
+            "mov" -> {
+                retval += "1-"
+            }
+            "trn" -> {
+                retval += "2-"
+            }
+            "sav" -> {
+                retval += "3-"
+            }
+            "lod" -> {
+                retval += "4-"
+            }
+            "add" -> {
+                retval += "5-"
+            }
+            "sub" -> {
+                retval += "6-"
+            }
+            "mul" -> {
+                retval += "7-"
+            }
             else -> throw InstructionException("An unknown instruction was encountered.")
         }
+        retval += "${intermediate[1]}-"
+        retval += "${intermediate[2]}-"
+        retval += "${intermediate[3]}|"
+        retval = retval.toByteArray().toString()
         return retval
     }
 
@@ -24,12 +51,11 @@ class Compiler {
      *
      */
     private fun readFile(fileName: String): String {
-        val list = ArrayList<String>()
+        var retval = ""
         File(fileName).forEachLine {
-            list.add(processLine(it))
+            retval += processLine(it)
         }
-        val array = arrayOfNulls<Array<String>>(list.size)
-        return list.toArray(array).toString()
+        return retval
     }
 
     /**
--- Simulator/src/main/kotlin/edu/drexel/se575/contract/Interpreter.kt
+++ Simulator/src/main/kotlin/edu/drexel/se575/contract/Interpreter.kt
@@ -4,11 +4,37 @@ package edu.drexel.se575.contract
  *
  */
 class Interpreter {
+    // Transient memory values.
+    private var memory = IntArray(1024)
+
+    // Persistent memory, accessible between runs.
+    private var storage = IntArray(1024) // TODO Find a way to define persistent storage.
 
     /**
      *
      */
-    fun runContract() {
-
+    fun runContract(contract: String) {
+        contract.split("|").forEach { instr ->
+            instr.split("-").forEach {
+                when (it.toInt()) {
+                    0 -> {
+                    }
+                    1 -> {
+                    }
+                    2 -> {
+                    }
+                    3 -> {
+                    }
+                    4 -> {
+                    }
+                    5 -> {
+                    }
+                    6 -> {
+                    }
+                    7 -> {
+                    }
+                }
+            }
+        }
     }
 }
