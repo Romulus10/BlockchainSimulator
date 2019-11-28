c874cb87a41af9612bb669099c4e1a78712c668f
Sean Batzel <romulus108@protonmail.com>
2019-11-28 18:33:21 -0500

    Started endpoints for blockchain interaction.

--- Simulator/src/main/kotlin/edu/drexel/se575/BlockchainSimulator.kt
+++ Simulator/src/main/kotlin/edu/drexel/se575/BlockchainSimulator.kt
@@ -6,5 +6,34 @@ fun main(args: Array<String>) {
     println("Starting node...")
 
     val app = Javalin.create().start(7000)
+
     app.get("/") { ctx -> ctx.result("Hello World") }
+
+    app.post("/transaction/create") { ctx ->
+        ctx.result("")
+    }
+
+    app.get("/transaction/list") { ctx ->
+        ctx.result("")
+    }
+
+    app.get("/transaction/list_by_block/:block_id") { ctx ->
+        ctx.result("")
+    }
+
+    app.get("/transaction/:tx_id") { ctx ->
+        ctx.result("")
+    }
+
+    app.post("/block/create") { ctx ->
+        ctx.result("")
+    }
+
+    app.get("/block/list") { ctx ->
+        ctx.result("")
+    }
+
+    app.get("/block/:tx_id") { ctx ->
+        ctx.result("")
+    }
 }
