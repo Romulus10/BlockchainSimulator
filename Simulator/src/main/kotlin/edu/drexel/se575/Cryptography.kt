cca3a9f65474c4d035d110f08e950e560b842933
Sean Batzel <romulus108@protonmail.com>
2019-11-30 15:31:51 -0500

    Crypto (#40)
    * GitIgnore update.
    
    * Basic logic for generating keypairs and signing/verifying. Just a warning, they probably don't work right yet.

---
+++ Simulator/src/main/kotlin/edu/drexel/se575/Cryptography.kt
@@ -0,0 +1,33 @@
+package edu.drexel.se575
+
+import java.security.*
+import java.util.*
+import java.util.Base64.getDecoder
+
+
+class Cryptography {
+    @Throws(Exception::class)
+    fun generateKeyPair(): KeyPair? {
+        val generator = KeyPairGenerator.getInstance("RSA")
+        generator.initialize(2048, SecureRandom())
+        return generator.generateKeyPair()
+    }
+
+    @Throws(Exception::class)
+    fun sign(plainText: String, privateKey: PrivateKey?): String? {
+        val privateSignature = Signature.getInstance("SHA256withRSA")
+        privateSignature.initSign(privateKey)
+        privateSignature.update(plainText.toByteArray())
+        val signature = privateSignature.sign()
+        return Base64.getEncoder().encodeToString(signature)
+    }
+
+    @Throws(Exception::class)
+    fun verify(plainText: String, signature: String?, publicKey: PublicKey?): Boolean {
+        val publicSignature = Signature.getInstance("SHA256withRSA")
+        publicSignature.initVerify(publicKey)
+        publicSignature.update(plainText.toByteArray())
+        val signatureBytes = getDecoder().decode(signature)
+        return publicSignature.verify(signatureBytes)
+    }
+}
