4d1a1be29e900cacae06072ac837a08c830dbd6c
Chris Vennel <35314989+cvennel@users.noreply.github.com>
2019-11-30 20:08:37 -0500

    Force transactions to be signed (#42)
    * connected Cryptography signatures to transactions. Forced transactions to be signed before adding to blockchain
    
    * Added PublicKeyDeserializer and signed transactions in unit tests

--- Simulator/src/test/kotlin/edu/drexel/se575/BlockTest.kt
+++ Simulator/src/test/kotlin/edu/drexel/se575/BlockTest.kt
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class BlockTests {
+    private val keyPair = Cryptography().generateKeyPair()
+
     @Test
     fun `mintNewBlock produces a valid block with the correct size`() {
-        val blockTxList: Array<Transaction> = arrayOf(Transaction("test", "test", "Text TX"))
+        val blockTxList: Array<Transaction> = arrayOf(Transaction("test", "test", "Text TX", keyPair!!.public))
         val block = Block(blockTxList, "test", "test", "000")
         assertEquals(block.transactions.size, 1)
     }
