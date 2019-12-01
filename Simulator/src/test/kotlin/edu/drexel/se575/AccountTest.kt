a78a099b1387fb4d38eb56d544fe25ad58b93240
Sean Batzel <romulus108@protonmail.com>
2019-12-01 10:24:22 -0500

    Removed some unnecessary test files and cleaned up logical errors dug up by new tests.

--- Simulator/src/test/kotlin/edu/drexel/se575/AccountTest.kt
+++ Simulator/src/test/kotlin/edu/drexel/se575/AccountTest.kt
@@ -1,18 +1,38 @@
 package edu.drexel.se575
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class AccountTest {
 
     @Test fun getAddress() {
+        val acct = Account()
+        acct.address = "some_address"
+        assertEquals(acct.address, "some_address")
     }
 
     @Test fun setAddress() {
+        val acct = Account()
+        acct.address = "some_address"
+        assertEquals(acct.address, "some_address")
     }
 
     @Test fun getWeight() {
+        val acct = Account()
+        acct.weight = 1
+        assertEquals(acct.weight, 1)
     }
 
     @Test fun setWeight() {
+        val acct = Account()
+        acct.weight = 1
+        assertEquals(acct.weight, 1)
+    }
+
+    @Test fun maxAccountWeight() {
+
+    }
+
+    @Test fun transferAccountValue() {
     }
 }
--- Simulator/src/test/kotlin/edu/drexel/se575/contract/InterpreterTest.kt
+++ Simulator/src/test/kotlin/edu/drexel/se575/contract/InterpreterTest.kt
@@ -9,4 +9,14 @@ internal class InterpreterTest {
         val result = Interpreter().runContract("5-0-1|5-1-1|1-2-1|12-2")
         assertEquals(result, 1)
     }
+
+    @Test fun `checks that a compiled contract will have the correct result`() {
+        val result = compile("""
+            add 0 1
+            add 1 1
+            mov 2 1
+            ret 2
+        """.trimIndent())
+        assertEquals(Interpreter().runContract(result), 1)
+    }
 }
