61d0c4ce713785ae87c976cf3d0d01b74a3c1ecc
unknown <tnl34@drexel.edu>
2019-12-04 17:46:45 -0500

    generated account service

--- Frontend/ionic-app/src/app/home/home.page.ts
+++ Frontend/ionic-app/src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { BlockService } from '../services/block.service';
 import { Observable } from 'rxjs';
 import { Block } from 'src/models/block';
+import { AccountService } from '../services/account.service';
 
 @Component({
   selector: 'app-home',
@@ -13,8 +14,13 @@ export class HomePage {
 
   constructor(
     protected blockService: BlockService,
+    protected accountService: AccountService,
   ) {
     this.blocks = this.blockService.getAllBlocks();
+  }
+
+  onCreateAccountClick() {
+    this.accountService.createAccount().subscribe();
   }
 
   onAddButtonClick() {
--- Frontend/ionic-app/src/app/services/account.service.ts
+++ Frontend/ionic-app/src/app/services/account.service.ts
@@ -1,26 +1,23 @@
 import { Injectable } from '@angular/core';
-import { Block } from 'src/models/block';
-import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class BlockService {
+export class AccountService {
   private readonly baseUrl: string = environment.baseUrl;
 
   constructor(
     protected http: HttpClient,
   ) { }
 
-  public getAllBlocks(): Observable<Block[]> {
-    // TODO: change to rest API call
-    const url = `${this.baseUrl}/client/block/list`;
-
-    return this.http.get<Block[]>(url).pipe(
-      tap(blocks => console.log('blocks', blocks))
-    );
+  createAccount(): Observable<any> {
+    const url = `${this.baseUrl}/client/account/create`;
+    return this.http.post(url, {}).pipe(
+      tap(res => console.log('account created', res))
+    )
   }
 }
