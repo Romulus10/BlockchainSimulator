5d7c33d9b22f5833c63223d11a07bfe3a97dc141
thejdubs <thejdubs2@aol.com>
2019-12-05 18:40:53 -0500

    remove dup

--- Frontend/ionic-app/src/app/services/transaction.service.ts
+++ Frontend/ionic-app/src/app/services/transaction.service.ts
@@ -29,11 +29,4 @@ export class TransactionService {
       tap(res => console.log('transaction list', res))
     )
   }
-
-  listTransactions(): Observable<Transaction[]> {
-    const url = `${this.baseUrl}/client/transaction/list`;
-    return this.http.get<Transaction[]>(url).pipe(
-      tap(res => console.log('transaction list', res))
-    )
-  }
 }
