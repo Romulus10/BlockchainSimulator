e986525b617f0ab68649510ec63d2d0159b59a0d
Chris Vennel <35314989+cvennel@users.noreply.github.com>
2019-12-05 10:00:27 -0500

    Update TransactionQueue.kt

--- Simulator/src/main/kotlin/edu/drexel/se575/TransactionQueue.kt
+++ Simulator/src/main/kotlin/edu/drexel/se575/TransactionQueue.kt
@@ -1,10 +1,11 @@
 package edu.drexel.se575
 
+val TX_PER_BLOCK = 5
 
 class TransactionQueue(private var blockChain: BlockChain) {
 
     private var transactions = arrayListOf<Transaction>()
-    val TX_PER_BLOCK = 5
+   
 
     fun addTransaction(transaction: Transaction) {
         transactions.add(transaction)
