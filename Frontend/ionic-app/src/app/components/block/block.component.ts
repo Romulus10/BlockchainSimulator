eca366c22344aafa785026c0949048ed2e476874
Sean Batzel <romulus108@protonmail.com>
2019-12-05 21:59:37 -0500

    I have no idea what I'm doing.

--- Frontend/ionic-app/src/app/components/block/block.component.ts
+++ Frontend/ionic-app/src/app/components/block/block.component.ts
@@ -10,6 +10,7 @@ import { Block } from 'src/models/block';
 export class BlockComponent implements OnInit {
   @Input() block: Block;
   @Input() index: number;
+  @Input() isValid: boolean;
   viewTransactions: boolean;
 
   constructor(
